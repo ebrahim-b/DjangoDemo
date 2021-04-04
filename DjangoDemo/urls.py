2f13f7f4c89032100d4be81a032a66494f848bf9
Ebrahim Barati Nasab <abrahim_barati@yahoo.com>
2021-04-04 20:53:34 +0430

    Separate urls for App

--- DjangoDemo/urls.py
+++ DjangoDemo/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from blog.views import display
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/',display)
+    path('',include('blog.urls'))
 ]
