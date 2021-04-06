bbb45af31d932bd9626c65a21ae37f5b259a2d28
Ebrahim Barati Nasab <abrahim_barati@yahoo.com>
2021-04-06 23:01:08 +0430

    Show data with templates

--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def display(request):
-    return HttpResponse('<h1>Tango With Django</h1>')
+    return render(request,'main.html')
